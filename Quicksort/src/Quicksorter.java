ae5af0b6f49908364afac6080ed1b9d84252b82f
DenniJensen <s0532338@htw-berlin.de>
2013-08-16 16:37:54 +0200

    modify Quicksorter and QuicksorterRunnable

--- Quicksort/src/Quicksorter.java
+++ Quicksort/src/Quicksorter.java
@@ -1,43 +1,52 @@
 /**
  * 
  * @author Dennis Haegler
- *
+ * 
  */
-public class Quicksort {
-	/** The array to sort.*/
+public class Quicksorter {
+	/** The array to sort. */
 	protected int[] array;
-	
-	/**Starting location to start from.*/ 
+
+	/** Starting location to start from. */
 	protected int start;
-	
-	/** Ending location to end with.*/
+
+	/** Ending location to end with. */
 	protected int end;
-	
-	/** The logger from loger4j to log information in the class*/
-	//private static //logger //logger = //logger.get//logger(Quicksort.class);
-	
+
 	/**
 	 * 
 	 * @param array
 	 */
-	public Quicksort(int[] array) {
+	public Quicksorter(int[] array) {
 		init(array, 0, array.length - 1);
 	}
-	
+
 	/**
 	 * 
 	 * @param array
 	 * @param start
 	 * @param end
 	 */
-	public Quicksort(int[] array, int start, int end) {
+	public Quicksorter(int[] array, int start, int end) {
 		init(array, start, end);
 	}
 
 	/**
+	 * 
+	 * @param array
+	 * @param start
+	 * @param end
+	 */
+	private void init(int[] array, int start, int end) {
+		this.array = array;
+		this.start = start;
+		this.end = end;
+	}
+	
+	/**
 	 * Runs the quicksort.
 	 */
-	public void run() {
+	public void quicksort() {
 		quicksort(start, end);
 	}
 
@@ -53,7 +62,6 @@ public class Quicksort {
 	 *            the end of the index in the array for the sort algorithm
 	 */
 	public void quicksort(int left, int right) {
-		//logger.info("Starts quickst from " + left + " to " + right);
 		int start = left;
 		int end = right;
 		int pivotPlace = (start + (end + 1)) / 2;
@@ -62,36 +70,24 @@ public class Quicksort {
 			while (array[left] <= pivotValue && left < end) {
 				left++;
 			}
-			//logger.debug("DETECT left " + array[left] + " on " + left + " is bigger than " + pivotValue);
 			while (array[right] >= pivotValue && start < right) {
 				right--;
 			}
-			//logger.info("DETECT right " + array[right] + " on " + right + " is smaller than " + pivotValue);
 			if (left < right) {
-				//TODO check on same values and diable -- or ++
-				//logger.debug("Swapped " + array[left] + " and " + array[right]);
 				swap(left, right);
-			} 
+			}
 			if (left == right) {
-				//logger.debug("Swapped Pivot" + array[left] 
-			//			+ " and " + array[pivotPlace]);
 				swap(left, pivotPlace);
 			}
 		}
-		//logger.debug("Ends sorting step. left->" + left + " right->" + right);
-		//logger.debug("Ends sorting step. start->" + start + " end->" + end);
-		//logger.debug(toString());
 		left--;
 		right++;
 		if ((left - start) >= 1) {
-			//logger.debug("LEFTSORT");
 			this.quicksort(start, left);
 		}
 		if ((end - right) >= 1) {
-			//logger.debug("RIGHTSORT");
 			this.quicksort(right, end);
 		}
-		//logger.debug(this.toString());
 	}
 
 	/**
@@ -106,7 +102,7 @@ public class Quicksort {
 		}
 		return string;
 	}
-	
+
 	/**
 	 * 
 	 * @return
@@ -114,7 +110,7 @@ public class Quicksort {
 	public boolean isSorted() {
 		boolean isSorted = false;
 		for (int i = 0; i < array.length - 1; i++) {
-			if(array[i] > array[i + 1]) {
+			if (array[i] > array[i + 1]) {
 				return isSorted;
 			}
 		}
@@ -133,22 +129,9 @@ public class Quicksort {
 	 * @param indexRight
 	 *            the index of one of the values to swap.
 	 */
-	protected void swap(int indexLeft, int indexRight) {
+	private void swap(int indexLeft, int indexRight) {
 		int buffer = array[indexLeft];
 		array[indexLeft] = array[indexRight];
 		array[indexRight] = buffer;
-	}
-	
-	/**
-	 * Initializes a Quicksort.
-	 * 
-	 * @param array
-	 * @param start
-	 * @param end
-	 */
-	private void init(int[] array, int start, int end) {
-		this.array = array;
-		this.start = start;
-		this.end = end;
 	}
 }
--- Quicksort/src/QuicksorterRunnable.java
+++ Quicksort/src/QuicksorterRunnable.java
@@ -1,82 +1,107 @@
-//import org.apache.log4j.logger;
+import java.util.ArrayList;
+import java.util.List;
 
-public class QuicksortRunnable extends Quicksort implements Runnable {
-	/** */
-	//private static Logger logger = logger.getlogger(QuicksortRunnable.class);
-	
-	/**
-	 * 
-	 * @param array
-	 */
-	public QuicksortRunnable(int[] array) {
-		super(array);
+public class QuicksorterRunnable implements Runnable {
+	int[] array;
+	int start;
+	int end;
+	int numberCores;
+
+	public QuicksorterRunnable(int[] array, int numberCores) {
+		init(array, 0, array.length, numberCores);
+		this.numberCores = numberCores;
+	}
+
+	public QuicksorterRunnable(int[] array, int start, int end, int numberCores) {
+		init(array, start, end, numberCores);
+	}
+
+	private void init(int[] array, int start, int end, final int numberCores) {
+		this.array = array;
+		this.start = start;
+		this.end = end;
+		this.numberCores = numberCores;
 	}
 	
 	/**
 	 * 
-	 * @param array
-	 * @param start
-	 * @param end
+	 * @return
 	 */
-	public QuicksortRunnable(int[] array, int start, int end) {
-		super(array,start, end);
+	public boolean isSorted() {
+		boolean isSorted = false;
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				return isSorted;
+			}
+		}
+		isSorted = true;
+		return isSorted;
 	}
-	
-	/**
-	 * 
-	 */
-	@Override
+
+
 	public void run() {
-		quicksort(start, end);
+		quicksort(start, end, numberCores);
 	}
-	
-	/**
-	 * 
-	 */
-	@Override
-	public void quicksort(int left, int right) {
-		//logger.info("Starts quickst from " + left + " to " + right);
+
+	public void quicksort(int left, int right, int numberCores) {
 		int start = left;
 		int end = right;
 		int pivotPlace = (start + (end + 1)) / 2;
 		int pivotValue = array[pivotPlace];
-		//logger.debug("start->" + array[left] + " end->" + array[right] 
-		//		+ " Pivot on: " + pivotPlace + " Value is: " + pivotValue);
-		//logger.debug(toString());
 		while (left < right) {
 			while (array[left] <= pivotValue && left < end) {
 				left++;
 			}
-			//logger.debug("DETECT left " + array[left] + " on " + left + " is bigger than " + pivotValue);
 			while (array[right] >= pivotValue && start < right) {
 				right--;
 			}
-			//logger.info("DETECT right " + array[right] + " on " + right + " is smaller than " + pivotValue);
 			if (left < right) {
-				//logger.debug("Swapped " + array[left] + " and " + array[right]);
 				swap(left, right);
-			} 
+			}
 			if (left == right) {
-				//logger.debug("Swapped Pivot" + array[left] 
-				//		+ " and " + array[pivotPlace]);
 				swap(left, pivotPlace);
 			}
 		}
-		//logger.debug("Ends sorting step. left->" + left + " right->" + right);
-		//logger.debug("Ends sorting step. start->" + start + " end->" + end);
-		//logger.debug(toString());
 		left--;
 		right++;
 		if ((left - start) >= 1) {
-			//logger.debug("LEFTSORT");
-			new Thread(new QuicksortRunnable(this.array, start, left)).start();
-			//this.quicksort(start, left);
+			// TODO start a new Runnable job
 		}
 		if ((end - right) >= 1) {
-			//logger.debug("RIGHTSORT");
-			new Thread(new QuicksortRunnable(this.array, right, end)).start();
-			//this.quicksort(right, end);
+			// TODO start a new Thread
 		}
-		//logger.debug(this.toString());
+
+		if (numberCores > 0) {
+			final List<Thread> threads = new ArrayList<Thread>();
+			if (left - start > 1) {
+				final Runnable runnable = new QuicksorterRunnable(array,
+						start, left - 1, numberCores - 1);
+				threads.add(new Thread(runnable));
+			}
+			if (end - right > 1) {
+				final Runnable runnable = new QuicksorterRunnable(array,
+						start, left - 1, numberCores - 1);
+				threads.add(new Thread(runnable));
+			}
+			for (final Thread thread : threads)
+				thread.start();
+			try {
+				for (final Thread thread : threads)
+					thread.join();
+			} catch (final InterruptedException e) {
+				e.printStackTrace();
+			}
+		} else {
+			if (left - start > 1)
+				quicksort(start, left - 1, numberCores);
+			if (end - right > 1)
+				quicksort(left + 1, end, numberCores);
+		}
 	}
- }
+
+	private void swap(int indexLeft, int indexRight) {
+		int buffer = array[indexLeft];
+		array[indexLeft] = array[indexRight];
+		array[indexRight] = buffer;
+	}
+}
